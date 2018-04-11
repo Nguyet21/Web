23c3aeb3d2cdf75be4742037896539088dba8db9
DUC93 <ducrocklee@gmail.com>
2018-04-11 14:06:04 +0700

    ok

---
+++ web/Areas/QuanTri/Controllers/TrChuQTController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.Areas.QuanTri.Controllers
+{
+    public class TrChuQTController : Controller
+    {
+        // GET: QuanTri/TrChuQT
+        public ActionResult TrangChuQT()
+        {
+            return View();
+        }
+    }
+}
